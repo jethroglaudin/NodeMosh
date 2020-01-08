7535ed471eed686c13216c9fa1bb71914464a16c
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-01-08 01:33:58 -0500

    used the os module to get information about the operating system, such as the total memory and free'd up memory available

--- first-app/app.js
+++ first-app/app.js
@@ -1,4 +1,9 @@
-// const path = require('path'); 
-// let pathObj = path.parse(__filename);
-// console.log(pathObj)
+const path = require('path'); 
+const os = require("os");
 
+let totalMemory = os.totalmem();
+let freeMemory = os.freemem();
+let pathObj = path.parse(__filename);
+
+console.log(`Total Memory: ${totalMemory}`);
+console.log(`Free Memory: ${freeMemory}`);
