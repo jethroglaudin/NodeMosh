39035bdff0f3c5adb2696999f7e894c9fe99c365
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-01-08 01:40:03 -0500

    file system practice working with asynch and synch methods for reading directories

--- first-app/app.js
+++ first-app/app.js
@@ -1,9 +1,22 @@
 const path = require('path'); 
 const os = require("os");
+const fs = require('fs');
+
+let pathObj = path.parse(__filename);
 
 let totalMemory = os.totalmem();
 let freeMemory = os.freemem();
-let pathObj = path.parse(__filename);
+
+
+// asynchronous approach
+const files = fs.readdirSync('./');
+
+// synchronous approach
+fs.readdir('./', function(err, files) {
+    if (err) console.log(`Error: ${error}`)
+    console.log('Result', files)
+})
 
 console.log(`Total Memory: ${totalMemory}`);
 console.log(`Free Memory: ${freeMemory}`);
+console.log(files);
