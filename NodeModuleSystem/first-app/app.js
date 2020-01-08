7c8a8637c194352bc8608f885bf3b1fac69ecbe3
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-01-08 01:56:05 -0500

    contiuing working with Event emmiters

--- NodeModuleSystem/first-app/app.js
+++ NodeModuleSystem/first-app/app.js
@@ -1,7 +1,8 @@
 const path = require('path'); 
 const os = require("os");
 const fs = require('fs');
-
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
 let pathObj = path.parse(__filename);
 
 let totalMemory = os.totalmem();
@@ -16,6 +17,22 @@ fs.readdir('./', function(err, files) {
     if (err) console.log(`Error: ${error}`)
     console.log('Result', files)
 })
+
+// Register a listener
+emitter.on('messageLogged', arg => {
+    console.log('Listener Called', arg)
+})
+// Raise an event
+emitter.emit('messageLogged', { id: 1, url: 'http://' });
+
+// Raise: logging (date: message);
+
+emitter.on('logging', eventArg => {
+    console.log('Logging Called: ', eventArg);
+});
+
+emitter.emit('logging', { id: 2, msg: "Here's some data" });
+
 
 console.log(`Total Memory: ${totalMemory}`);
 console.log(`Free Memory: ${freeMemory}`);
