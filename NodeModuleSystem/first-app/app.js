24bb8e385f4cce440026e4108f0964f54dbbb7a2
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-01-08 23:20:26 -0500

    practice with the HTTP Module

--- NodeModuleSystem/first-app/app.js
+++ NodeModuleSystem/first-app/app.js
@@ -4,7 +4,23 @@ const fs = require('fs');
 const EventEmitter = require('events');
 const Logger = require('./logger');
 const logger = new Logger();
+const http = require("http");
 
+const server = http.createServer((req, res) => {
+    if(req.url === '/'){
+        res.write("Hello World")
+        res.end();
+    }
+
+    if(req.url === '/api/courses'){
+        res.write(JSON.stringify([1,2,3]));
+        res.end();
+    }
+}); 
+
+server.listen(3000);
+
+console.log('Listening on port 3000...');
 // let pathObj = path.parse(__filename);
 // let totalMemory = os.totalmem();
 // let freeMemory = os.freemem();
@@ -26,13 +42,13 @@ const logger = new Logger();
 
 
 
-logger.on('messageLogged', arg => {
-    console.log('Listener Called', arg)
-});
+// logger.on('messageLogged', arg => {
+//     console.log('Listener Called', arg)
+// });
 
-// Raise: logging (date: message);
-logger.on('logging', eventArg => {
-    console.log('Logging Called: ', eventArg);
-});
+// // Raise: logging (date: message);
+// logger.on('logging', eventArg => {
+//     console.log('Logging Called: ', eventArg);
+// });
 
-logger.log('message');
+// logger.log('message');
