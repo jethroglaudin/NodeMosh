216a003522cdc2de4008bbe2eddbdda89bfeac10
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-01-08 23:01:21 -0500

    updated app.js and logger.js

--- NodeModuleSystem/first-app/app.js
+++ NodeModuleSystem/first-app/app.js
@@ -2,38 +2,37 @@ const path = require('path');
 const os = require("os");
 const fs = require('fs');
 const EventEmitter = require('events');
-const emitter = new EventEmitter();
-let pathObj = path.parse(__filename);
+const Logger = require('./logger');
+const logger = new Logger();
 
-let totalMemory = os.totalmem();
-let freeMemory = os.freemem();
+// let pathObj = path.parse(__filename);
+// let totalMemory = os.totalmem();
+// let freeMemory = os.freemem();
 
+// // console.log(`Total Memory: ${totalMemory}`);
+// // console.log(`Free Memory: ${freeMemory}`);
+// // console.log(files);
 
-// asynchronous approach
-const files = fs.readdirSync('./');
+// // asynchronous approach
+// const files = fs.readdirSync('./');
 
-// synchronous approach
-fs.readdir('./', function(err, files) {
-    if (err) console.log(`Error: ${error}`)
-    console.log('Result', files)
-})
+// // synchronous approach
+// fs.readdir('./', function(err, files) {
+//     if (err) console.log(`Error: ${error}`)
+//     console.log('Result', files)
+// })
 
 // Register a listener
-emitter.on('messageLogged', arg => {
+
+
+
+logger.on('messageLogged', arg => {
     console.log('Listener Called', arg)
-})
-// Raise an event
-emitter.emit('messageLogged', { id: 1, url: 'http://' });
+});
 
 // Raise: logging (date: message);
-
-emitter.on('logging', eventArg => {
+logger.on('logging', eventArg => {
     console.log('Logging Called: ', eventArg);
 });
 
-emitter.emit('logging', { id: 2, msg: "Here's some data" });
-
-
-console.log(`Total Memory: ${totalMemory}`);
-console.log(`Free Memory: ${freeMemory}`);
-console.log(files);
+logger.log('message');
--- NodeModuleSystem/first-app/logger.js
+++ NodeModuleSystem/first-app/logger.js
@@ -1,8 +1,19 @@
+const EventEmitter = require('events');
+
 let url = 'http://mylogger.io/log';
 
-function log(message) {
-    // Send an Http Request
-    console.log(message);
-}
+class Logger extends EventEmitter {
+    log(message) {
+        // Send an Http Request
+        console.log(message);
+    
+    // Raise an event
+    this.emit('messageLogged', { id: 1, url: 'http://' });
+    this.emit('logging', { id: 2, msg: "Here's some data" });
+    }
 
-module.exports = log;
+} 
+
+
+
+module.exports = Logger;
