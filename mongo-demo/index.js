fa3fa8fadce7aec3b7503aef5fbaddd80f903779
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-01-13 12:34:25 -0500

    created schema

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -3,4 +3,20 @@ const mongoose = require("mongoose");
 mongoose
   .connect("mongodb://localhost/playground")
   .then(() => console.log("Connected to MongoDB...."))
-  .catch(err => console.log('Could not connect to MongoDB...', err));
+  .catch(err => console.log("Could not connect to MongoDB...", err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean
+});
+
+const Course = mongoose.model("Course", courseSchema);
+const course = new Course({
+  name: "Node.js Course",
+  author: "Jethro",
+  tags: ["node", "backend"],
+  isPublished: true
+});
