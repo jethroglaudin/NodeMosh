6ea4a2b1af238016604f68aa205d796573630257
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-01-13 13:07:55 -0500

    working with Comparison query operations

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -28,7 +28,8 @@ async function createCourse() {
 }
 
 async function getCourses() {
-  const courses = await Course.find({ author: "Jethro", isPublished: true })
+  const courses = await Course
+    .find({ author: "Jethro", isPublished: true })
     .limit(10)
     .sort({ name: 1 })
     .select({ name: 1, tags: 1 })
