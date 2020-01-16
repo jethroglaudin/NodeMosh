77bdcddb18ff7155625d9349d692e522d0c59be5
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-01-15 23:18:56 -0500

    updated index_2.js

--- mongo-demo/index_2.js
+++ mongo-demo/index_2.js
@@ -1,3 +1,6 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/mongo-exercises").then(() => console.log("Connected to DB")).catch(err => console.log(err))
+mongoose
+  .connect("mongodb://localhost/mongo-exercises")
+  .then(() => console.log("Connected to DB"))
+  .catch(err => console.log(err));
