86366d8169c8594da7de4746be48c8daffe3d526
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-02-04 14:51:22 -0500

    updated schema added gernateToken function

--- vidly/models/User.js
+++ vidly/models/User.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const Joi = require("joi");
+const config = require('config');
+const jwt = require('jsonwebtoken');
 
-const User = mongoose.model('User', new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -22,7 +24,13 @@ const User = mongoose.model('User', new mongoose.Schema({
         maxLength: 1024,
         required: true
     }
-}))
+});
+
+userSchema.methods.generateAuthToken = function() {
+    const token = jwt.sign({ _id: this._id }, config.get('jwtPrivateKey'));
+    return token;
+}
+const User = mongoose.model('User', userSchema)
 
 function validateUser(user){
     const schema = {
