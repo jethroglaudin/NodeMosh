640f2e7d73619d6e5f00a7ea7858665a6680a1ae
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-02-04 13:48:30 -0500

    updated router.post for registering new user. password is now hashed using bcrypt

---
+++ vidly/x.js
@@ -0,0 +1,15 @@
+// Authentication and Authorization
+
+/* Authentication is the process of indentifying the user to make sure they are who 
+   they say they are. Authorization determins if the user has the right permission
+   to perform the end point route */
+
+   // Register: POST /api/users
+   // LOGIN: POST /api/logins
+
+   // 
+
+   email: {
+       type: String,
+       unique: true
+   }
